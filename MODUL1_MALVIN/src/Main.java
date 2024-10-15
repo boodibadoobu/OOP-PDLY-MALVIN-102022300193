3c4e758b40cb5d30b52f72f56cc8ab912c4106d1
boodibadoobu <nailvinz@gmail.com>
2024-10-15 15:32:05 +0700

    Add files via upload

---
+++ MODUL1_MALVIN/src/Main.java
@@ -0,0 +1,9 @@
+// Don't delete any comments below!!!
+
+public class Main {
+    public static void main(String[] args) {
+        // Todo : Create ManajemenInventaris Object and Scanner
+        
+        // Todo : Create Loop list menu
+    }
+}
